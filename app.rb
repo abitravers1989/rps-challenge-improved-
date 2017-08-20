21e1c53bf960e8f2830d2b439bbc4b866a39b5ae
Abi Travers <atravers4007@gmail.com>
2017-08-20 13:41:11 +0100

    Testing all aspects and improving welcome page where player inputs name and selects to start the game

--- app.rb
+++ app.rb
@@ -10,20 +10,26 @@ class MyApp < Sinatra::Base
     erb :index
   end
 
-  post '/' do
-    session[:name] = params[:name]
-    redirect '/play'
-  end
-
-  get '/play' do
-    @name = session[:name]
-    erb :play
-  end
-
   post '/play' do
-  session[]
-  redirect 'selected_option'
+    p params
+    session[:name] = params[:name]
+    # redirect '/play'
   end
+
+  # get '/play' do
+  #   p params
+  #   @name = session[:name]
+  #   erb :play
+  # end
+  #
+  # post '/play' do
+  #   p params
+  #   redirect 'selected_option'
+  # end
+  #
+  # get '/selected_option'
+  #   dsdsd
+  # end
 
   run! if app_file == $0
   # Only run the following code when this file is the main file being run
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,5 @@
 
-def sign_in_and_play 
+def sign_in_and_play
   visit "/"
-  fill_in('name', with: 'waterbottle')
-  click_button('Submit')
+  
 end
